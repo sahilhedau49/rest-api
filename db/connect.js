39a3d7a5e2925ccff2338d94eefb0eea7a5edfae
sahilhedau49 <hedausahil49@gmail.com>
2023-06-23 21:30:13 +0530

    env updated

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-uri =
-  "mongodb+srv://hedausahil49:eX9KKixk6EABhlc3@restapi.8qxm1ng.mongodb.net/RestApi?retryWrites=true&w=majority";
-
-const connectDB = () => {
+const connectDB = (uri) => {
   console.log("Mongoooooooooooooooo!");
   return mongoose.connect(uri, {
     useNewUrlParser: true,
